eda57e2101da45796aeb3535a878d17a1ebaf02f
modestdavid <67259469+modestdavid@users.noreply.github.com>
2020-06-25 03:40:32 +0100

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,25 @@
+"""GCOM URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
+
+urlpatterns = [
+                  url(r'^sa/', include('shuup.admin.urls', namespace="shuup_admin", app_name="shuup_admin")),
+                  url(r'^api/', include('shuup.api.urls')),
+                  url(r'^', include('shuup.front.urls', namespace="shuup", app_name="shuup")),
+                  url(r'^repairer/', include('GCOM_APP.urls', app_name="repairer")),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
